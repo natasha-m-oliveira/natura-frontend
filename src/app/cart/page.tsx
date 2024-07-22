7e06b168af7450ae63088e29ffa73c3aacf877ec
natasha-m-oliveira <me@natashamo.dev>
2024-07-22 01:55:24 -0300

    feat: create Cart Page with Cart Management Features

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,85 @@
+'use client'
+
+import { Breadcrumbs } from '../../components/breadcrumbs'
+import Link from 'next/link'
+import { ProductDetails } from '../../components/product-details'
+import { ArrowRight, Tag } from '@phosphor-icons/react/dist/ssr'
+import { useShoppingCart } from '../../hooks/use-cart'
+import { formatToBRL } from '../../utils/price'
+
+export default function Cart() {
+  const { cartDetails, totalPrice, totalDiscount } = useShoppingCart()
+  const shipping = totalPrice ? 1500 : 0
+  const totalPurchase = totalPrice - totalDiscount + shipping
+
+  return (
+    <div className="w-[calc(100%-2rem)] max-w-[calc(1280px-2rem)] mx-auto py-2 border-t-[1px] border-zinc-200">
+      <Breadcrumbs>
+        <Link href="./">Início</Link>
+        <Link href="./cart">Carrinho</Link>
+      </Breadcrumbs>
+
+      <main className="grid grid-cols-[2fr_1fr] gap-4 my-4">
+        <h2 className="text-3xl font-extrabold mb-2 col-span-full">
+          Seu carrinho
+        </h2>
+
+        <div className="border-[1px] border-zinc-300 rounded-2xl divide-y px-4">
+          {cartDetails.products.length ? (
+            cartDetails.products.map((product) => (
+              <ProductDetails
+                key={product.id}
+                id={product.id}
+                name={product.name}
+                image={product.image}
+                price={product.price}
+                discount={product.discount}
+                quantity={product.quantity}
+              />
+            ))
+          ) : (
+            <span>Você não possui nenhum produto no carrinho.</span>
+          )}
+        </div>
+
+        <aside className="grid grid-cols-[1fr_auto] gap-y-4 gap-x-2 h-min border-[1px] border-zinc-300 rounded-2xl px-4 py-3">
+          <h3 className="text-xl font-extrabold col-span-full">Sumário</h3>
+          <span className="text-zinc-400">Subtotal</span>
+          <strong className="text-right">{formatToBRL(totalPrice)}</strong>
+
+          <span className="text-zinc-400">Desconto{!!totalDiscount}</span>
+          <strong className="text-right text-red-500">
+            -{formatToBRL(totalDiscount)}
+          </strong>
+
+          <span className="text-zinc-400">Frete</span>
+          <strong className="text-right">{formatToBRL(shipping)}</strong>
+
+          <div className="border-t-[1px] border-zinc-300 col-span-full" />
+
+          <span className="text-xl">Total</span>
+          <strong className="text-xl text-right">
+            {formatToBRL(totalPurchase)}
+          </strong>
+
+          <div className="bg-zinc-200 flex items-center gap-3 p-2 rounded-full">
+            <Tag aria-hidden className="text-zinc-500 w-6 h-6" weight="bold" />
+            <input
+              type="text"
+              placeholder="Cupom"
+              className="bg-transparent outline-none text-lg w-full"
+            />
+          </div>
+
+          <button className="bg-black text-white text-sm rounded-full py-2 px-3">
+            Aplicar
+          </button>
+
+          <button className="flex items-center justify-center gap-4 bg-orange-400 text-white text-sm col-span-full rounded-full py-3 px-4">
+            Finalizar compra <ArrowRight weight="bold" />
+          </button>
+        </aside>
+      </main>
+    </div>
+  )
+}
--- src/components/product-details.tsx
+++ src/components/product-details.tsx
@@ -1,13 +1,39 @@
 import Image from 'next/image'
 import NumberInput from './number-input'
 import { Trash } from '@phosphor-icons/react/dist/ssr'
+import { formatToBRL } from '../utils/price'
+import { useShoppingCart } from '../hooks/use-cart'
+import { resolveImageUrl } from '../utils/resolve-image-url'
 
-export function ProductDetails() {
+type ProductDetailsProps = {
+  id: string
+  name: string
+  image: string | null
+  price: number
+  discount: number | null
+  quantity: number
+}
+
+export function ProductDetails(props: ProductDetailsProps) {
+  const { removeItem, incrementItem, decrementItem } = useShoppingCart()
+
+  const currentPrice = props.discount
+    ? props.price - props.discount
+    : props.price
+
+  function handleQuantityChange(id: string, current: number, previous: number) {
+    if (current > previous) {
+      incrementItem(id)
+    } else if (current < previous) {
+      decrementItem(id)
+    }
+  }
+
   return (
     <div className="grid grid-cols-[auto_1fr_auto] gap-4 items-center py-3">
       <div className="flex items-center justify-center max-w-28 rounded-xl aspect-square">
         <Image
-          src="/no-image-available.jpg"
+          src={resolveImageUrl(props.image)}
           alt=""
           width={300}
           height={300}
@@ -15,14 +41,30 @@ export function ProductDetails() {
         />
       </div>
 
-      <div className="grid gap-3">
-        <span className="font-bold">Essencial Masculino 100ml</span>
-        <strong className="text-lg">R$212,00</strong>
+      <div>
+        <span className="font-bold">{props.name}</span>
+        <div className="relative my-3">
+          {!!props.discount && (
+            <strong className="block text-zinc-400 font-bold line-through">
+              {formatToBRL(props.discount)}
+            </strong>
+          )}
+          <strong className="block text-lg">{formatToBRL(currentPrice)}</strong>
+        </div>
       </div>
 
       <div className="flex flex-col justify-between items-end h-full">
-        <Trash className="text-red-500 text-xl" weight="fill" />
-        <NumberInput value={1} />
+        <Trash
+          className="text-red-500 text-xl cursor-pointer"
+          weight="fill"
+          onClick={() => removeItem(props.id)}
+        />
+        <NumberInput
+          value={props.quantity}
+          onValueChange={(value) =>
+            handleQuantityChange(props.id, value, props.quantity)
+          }
+        />
       </div>
     </div>
   )
